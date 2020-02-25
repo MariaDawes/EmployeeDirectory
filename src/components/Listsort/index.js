639ef483a5e1be20b7abf1f06367479af18970c6
MariaDawes <maria.c.dawes@gmail.com>
2020-02-25 12:48:19 -0600

    changed line style format

--- src/components/Listsort/index.js
+++ src/components/Listsort/index.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { Container, Row, Col } from "../Grid";
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Listsort(props) {
   return (
     <ul className="list-group">
       {props.employeeArraysort.map(item => (
-         <li className="list-group-item" key={item.id}>
-         <img className="img-fluid" alt="Smiley" src={item.image}></img>
-         <h3>{item.name}</h3> 
-         <h5>Email: {item.email}</h5>
-         <h5>City: {item.city}</h5>
-         <h5>Phone: {item.phone}</h5>
-         <h5>Employee id: {item.id}</h5>
-        </li>
+        <li className="list-group-item" key={item.id}>
+          <Container>
+            <Row>
+              <Col size="xs-2 sm-2">
+                <img className="img-fluid" alt="Smiley" src={item.image}></img>
+              </Col>
+              <Col size="xs-2 sm-2">
+                  <h6>{item.name}</h6></Col>
+              <Col size="xs-2 sm-2">
+                  <h6>{item.email}</h6></Col>
+              <Col size="xs-2 sm-2">
+                  <h6>{item.phone}</h6></Col>
+              <Col size="xs-2 sm-2">
+                  <h6>{item.empid}</h6></Col>
+              <Col size="xs-2 sm-2">
+                  <h6>{item.city}</h6></Col>
+            </Row>
+          </Container>
+       </li>
       ))}
     </ul>
   );
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,16 +8,16 @@ import Listsearch from "../components/Listsearch";
 
 
 const employeeArraysearch = [
-  { id: 1, name: 'Zena Wayne', email: 'janewayne@aaa.com', phone: '(775)763-8844', city: 'Austin', image: 'https://www.smileyhuette.de/images/smiley-facebook.png' },
-  { id: 2, name: 'Maria Crystal', email: 'mariacrystal@aaa.com', phone: '(555)565-5555', city: 'Boston', image: 'https://i.pinimg.com/originals/cc/e8/f5/cce8f5e2ce356473c5a9eb6268d66a1d.png' },
-  { id: 3, name: 'John Rodriguez', email: 'johnrodriguez@aaa.com', phone: '(775)763-8832', city: 'Austin' , image: 'https://npolitan.files.wordpress.com/2012/01/reminder-smiley-face1.jpg' },
-  { id: 4, name: 'Greta Gatsby', email: 'gretagatsby@aaa.com', phone: '(555)555-55533', city: 'Dublin' , image: 'https://i.ebayimg.com/images/g/osQAAOSwbwlXBS-T/s-l300.jpg' },
-  { id: 5, name: 'Mary Anne', email: 'maryanne@aaa.com', phone: '(775)763-8844', city: 'Dublin' , image: 'https://i.pinimg.com/736x/e1/85/21/e185219fc9115c994b26ce2e2d7c0b79--emoticon-emoji.jpg' },
-  { id: 6, name: 'Mary Claire Fusilier', email: 'marycfusilier@aaa.com', phone: '(555)555-5233', city: 'Dublin' , image: 'http://1.bp.blogspot.com/-y6yeKESK36k/U50pXu9e5bI/AAAAAAAAJU4/Q5t0xC3HxNI/s1600/business-emoticon.png' },
-  { id: 7, name: 'Melina Dawes', email: 'melinadawes@aaa.com', phone: '(555)535-5555', city: 'Paris' , image: 'https://i.pinimg.com/736x/04/00/d3/0400d3607a876354808bc844b44f47a2.jpg' },
-  { id: 8, name: 'Newton Acosta', email: 'newtonacosta@aaa.com', phone: '(775)743-8844', city: 'Boston' , image: 'http://3.bp.blogspot.com/-1LnCCxHvWy0/Vo2fUMCSmII/AAAAAAAAB2w/dya0ZLKj9Jk/s1600/business-call-smiley.png' },
-  { id: 9, name: 'Sonia Cabo', email: 'soniacabo@aaa.com', phone: '(555)555-8555', city: 'Austin' , image: 'https://i.pinimg.com/736x/af/ce/2b/afce2b000e96a45f09bce2c9213ed0a5--money-emoji-smiley-faces.jpg' },
-  { id: 10, name: 'Leonardo Cabo', email: 'leonardocabo@aaa.com', phone: '(775)773-8844', city: 'Austin' , image: 'https://3.bp.blogspot.com/-_4WEj_94u_s/WHuVTWhu2QI/AAAAAAAATbQ/6m5CHrhy3fo_k7zklVmyvAitUuAUb9JnwCLcB/s1600/bow-tie-smiley.png' }  
+  { id: 1, empid:'111111115-3', name: 'Zena Wayne', email: 'janewayne@aaa.com', phone: '+1(775)763-8844', city: 'Austin Office', image: 'https://www.smileyhuette.de/images/smiley-facebook.png' },
+  { id: 2, empid:'222555335-3', name: 'Maria Crystal', email: 'mariacrystal@aaa.com', phone: '+1(555)565-5555', city: 'Boston Office', image: 'https://i.pinimg.com/originals/cc/e8/f5/cce8f5e2ce356473c5a9eb6268d66a1d.png' },
+  { id: 3, empid:'365555335-3', name: 'John Rodriguez', email: 'johnrodriguez@aaa.com', phone: '+1(775)763-8832', city: 'Austin Office' , image: 'https://npolitan.files.wordpress.com/2012/01/reminder-smiley-face1.jpg' },
+  { id: 4, empid:'465786477-4', name: 'Greta Gatsby', email: 'gretagatsby@aaa.com', phone: '+1(555)555-55533', city: 'Dublin Office' , image: 'https://i.ebayimg.com/images/g/osQAAOSwbwlXBS-T/s-l300.jpg' },
+  { id: 5, empid:'590086543-5', name: 'Mary Anne', email: 'maryanne@aaa.com', phone: '+1(775)763-8844', city: 'Dublin Office' , image: 'https://i.pinimg.com/736x/e1/85/21/e185219fc9115c994b26ce2e2d7c0b79--emoticon-emoji.jpg' },
+  { id: 6, empid:'645653783-6', name: 'Mary Jones', email: 'marycfusilier@aaa.com', phone: '+1(555)555-5233', city: 'Dublin Office ' , image: 'http://1.bp.blogspot.com/-y6yeKESK36k/U50pXu9e5bI/AAAAAAAAJU4/Q5t0xC3HxNI/s1600/business-emoticon.png' },
+  { id: 7, empid:'711467947-7', name: 'Melina Dawes', email: 'melinadawes@aaa.com', phone: '+1(555)535-5555', city: 'Paris Office' , image: 'https://i.pinimg.com/736x/04/00/d3/0400d3607a876354808bc844b44f47a2.jpg' },
+  { id: 8, empid:'888645644-8', name: 'Newton Acosta', email: 'newtonacosta@aaa.com', phone: '+1(775)743-8844', city: 'Boston Office' , image: 'http://3.bp.blogspot.com/-1LnCCxHvWy0/Vo2fUMCSmII/AAAAAAAAB2w/dya0ZLKj9Jk/s1600/business-call-smiley.png' },
+  { id: 9, empid:'934566566-3', name: 'Sonia do Cabo', email: 'soniacabo@aaa.com', phone: '+1(555)555-8555', city: 'Brazil Office' , image: 'https://i.pinimg.com/736x/af/ce/2b/afce2b000e96a45f09bce2c9213ed0a5--money-emoji-smiley-faces.jpg' },
+  { id: 10, empid:'107433740-1', name: 'Leo do Cabo', email: 'leonardocabo@aaa.com', phone: '+1(775)773-8844', city: 'Brazil Office' , image: 'https://3.bp.blogspot.com/-_4WEj_94u_s/WHuVTWhu2QI/AAAAAAAATbQ/6m5CHrhy3fo_k7zklVmyvAitUuAUb9JnwCLcB/s1600/bow-tie-smiley.png' }  
 ];
 
 
@@ -34,7 +34,7 @@ function Search() {
   };
   
   const getFiltered = () => {
-    return employeeArraysearch.filter(employee => employee.name == thename)
+    return employeeArraysearch.filter(employee => employee.name === thename)
     }
 
 
